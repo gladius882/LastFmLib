0ed23d166b311b3a72d864070f0391dcfe027bae
gladius882 <=>
2018-03-19 20:44:19 +0100

    Initial commit. Tag class with properties

---
+++ LastFmLib/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: gladius882
+ * Date: 2018-03-19
+ * Time: 20:32
+ * 
+ */
+using System;
+
+namespace LastFmLib
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
---
+++ LastFmLib/Tag.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: gladius882
+ * Date: 2018-03-19
+ * Time: 20:35
+ * 
+ */
+using System;
+
+namespace LastFmLib
+{
+	/// <summary>
+	/// Description of Tag.
+	/// </summary>
+	public class Tag
+	{
+		public string Name
+		{
+			get; private set;
+		}
+		
+		public Tag()
+		{
+		}
+	}
+}
